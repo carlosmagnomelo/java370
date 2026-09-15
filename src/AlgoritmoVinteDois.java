f813c971fcfa1f24742e95ca1ee571cd603d13f8
carlosmagnomelo <carlosmagnomelo@hotmail.com>
2026-09-14 23:29:27 -0300

    terminei o 22

--- src/AlgoritmoVinteDois.java
+++ src/AlgoritmoVinteDois.java
@@ -12,7 +12,7 @@ public class AlgoritmoVinteDois {
         Mostre o valor total do financiamento, o valor financiado e a mensagem "Parabéns, seja bem vindo a família SENATECH. */
 
         int tentativas = 0;
-        int parcelas = 0;
+        
 
         while (tentativas < 3) 
         {
@@ -22,13 +22,8 @@ public class AlgoritmoVinteDois {
             if (nome.equals("senai") && senha.equals("123")) 
             {
                 System.out.println("login com sucesso");
-
-                IO.readln("Emprestimo máximo é 20.000| Quantas parcelas? ");
-                i++
-                double valor = Double.parseDouble(IO.readln("Digite o valor R$ "));
-                valor = valor * 1/100;
-              
-              break;   //serve para sair do sistema
+                
+                break;   //serve para sair do sistema
               
             }
           
@@ -39,10 +34,57 @@ public class AlgoritmoVinteDois {
             }
         }
             
-            if (tentativas == 3)     
-            {
-                System.out.println("cartão bloqueado, procure o gerente!");    
-            }
-        
+        if (tentativas == 3)     
+        {
+            System.out.println("cartão bloqueado, procure o gerente!");
+            return;    
+        }
+
+        // Emprestimo
+        IO.println("\n----- SENATECH -----");
+        IO.println("Bem-vindo ao simulador de empréstimo");
+
+        // VALIDAÇÃO DO VALOR
+        double valor = Double.parseDouble(IO.readln("Digite o valor do emprestimo R$ "));
+        if (valor <=0 || valor > 20000) 
+        {
+            IO.println("Valor invalido");
+            IO.println("O emprestimo máximo é R$ 20.000");
+            return;
+        }
+        // PARCELAS
+        int parcelas = Integer.parseInt(IO.readln("DIgite a quantidade de parcelas. Maximo 10 meses: "));
+        // VALIDAÇÃO DAS PARCELAS
+        if (parcelas <=0 || parcelas >=10) 
+        {
+            IO.println("Quantidade de parcelas inválida!");
+            IO.println("Escolha entre 1 e 10 parcelas.");
+
+            return;
+        }
+
+        double juros = 0.01;
+
+        double valorJuros = valor * juros * parcelas;
+
+        double valorTotal = valor + valorJuros;
+
+        double valorParcela = valorTotal / parcelas;
+
+        IO.println("\n===== RESULTADO =====");
+
+        IO.println("Valor financiado: R$ "+ String.format("%.2f", valor));
+
+        IO.println("Quantidade de parcelas: "+ parcelas + " meses");
+
+        IO.println("Juros: 1% ao mês");
+
+        IO.println("Valor dos juros: R$ "+ String.format("%.2f", valorJuros));
+
+        IO.println("Valor da parcela: R$ "+ String.format("%.2f", valorParcela));
+
+        IO.println("Valor total do financiamento: R$ "+ String.format("%.2f", valorTotal));
+
+        IO.println("\nParabéns, seja bem vindo a família SENATECH!");
     }
 }
